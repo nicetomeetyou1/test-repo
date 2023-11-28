24d8a5632ff85423e678eeff1c97f455d53a5072
nicetomeetyou1 <han@scallop.io>
2023-11-29 03:13:59 +0700

    feat: add mini wallet

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,2 @@
 export {default as Button} from './zklogin/button';
+export {default as MiniWallet} from './miniWallet';
---
+++ src/components/miniWallet.tsx
@@ -0,0 +1,37 @@
+import { type FC } from "react";
+
+type MiniWalletProps = {
+};
+
+const MiniWallet: FC<MiniWalletProps> = (() => {
+  return (
+    <div className="w-full h-full flex justify-center items-center">
+      <div className="w-[320px] bg-black/50 flex flex-col">
+        <div className="header">
+          <div className="w-full flex justify-center items-center p-2">
+            0xabcdefghijklmnopqrstuvwxyz
+          </div>
+          <div className="w-full flex justify-center items-center h-4">
+            <span>1,205 USD</span>
+          </div>
+        </div>
+        <div className="token-list flex flex-col">
+          <div className="w-full bg-white/40 p-2 flex justify-start items-center gap-2">
+            <div className="w-full">
+              <div className="w-3 h-3 rounded-full bg-red-200"></div>
+            </div>
+            <div className="w-full flex flex-col">
+              <span className="text-sm text-yellow-300">ETH</span>
+              <span className="text-sm text-white">≈ 2,048 USD</span>
+            </div>
+            <div className="w-full flex justify-end items-center">
+              <span className="text-sm text-green-400/50">0,1567</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+});
+
+export default MiniWallet;
